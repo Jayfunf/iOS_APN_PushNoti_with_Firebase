be0ed192088b10ebb204a3d92c4919b5de4bb5c4
Jayfunf <simh3077@gmail.com>
2022-09-26 14:00:07 +0900

    init

---
+++ iOS_PushNoti/iOS_PushNoti/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS_PushNoti
+//
+//  Created by Minhyun Cho on 2022/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS_PushNoti/iOS_PushNoti/iOS_PushNotiApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_PushNotiApp.swift
+//  iOS_PushNoti
+//
+//  Created by Minhyun Cho on 2022/09/26.
+//
+
+import SwiftUI
+
+@main
+struct iOS_PushNotiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
